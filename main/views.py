33debcab1bc5234be43e2d4adc988fdb18a872a5
mhmmdmadhanip <muhammad.madhani@ui.ac.id>
2024-05-02 01:42:35 +0700

    Login page

--- main/views.py
+++ main/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+
+@login_required(login_url='/login')
 
 # Create your views here.
 def show_main(request):
