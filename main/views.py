d8475e08d7ea3ae12452788701254b15138430bc
mhmmdmadhanip <muhammad.madhani@ui.ac.id>
2024-05-15 01:03:59 +0700

    Registrasi Akun tanpa Role dan Akun Podcaster

--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db import connection
+# import uuid
 
 
 # @login_required(login_url='/login')
@@ -59,6 +60,59 @@ def register_label(request):
     return render (request, 'register_label.html')
 
 def register_pengguna(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        nama = request.POST.get('nama')
+        gender = request.POST.get('gender')
+        if gender == "Laki-laki":
+            gender = 1
+        else :
+            gender = 0
+        tempat_lahir = request.POST.get('tempatLahir')
+        tanggal_lahir = request.POST.get('tanggalLahir')
+        kota_asal = request.POST.get('kotaAsal')
+        roles = request.POST.getlist('role')
+
+        print(f'Email: {email}')
+        print(f'Password: {password}')
+        print(f'Nama: {nama}')
+        print(f'Gender: {gender}')
+        print(f'Tempat Lahir: {tempat_lahir}')
+        print(f'Tanggal Lahir: {tanggal_lahir}')
+        print(f'Kota Asal: {kota_asal}')
+        print(f'Roles: {roles}')
+
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT EMAIL FROM AKUN WHERE email = %s", [email])
+            row = cursor.fetchone()
+        if row != None :
+            return HttpResponse("Email Already Registered")
+        else :
+            is_verified = False
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                    INSERT INTO AKUN (email, password, nama, gender, tempat_lahir, tanggal_lahir, is_verified, kota_asal)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                """, [email, password, nama, gender, tempat_lahir, tanggal_lahir, is_verified, kota_asal])
+            if roles.length() > 0 :
+                # new_uuid = uuid.uuid4()
+                for role in roles: 
+                    role = role.upper()
+                    if(role == "PODCASTER"):
+                        with connection.cursor() as cursor:
+                            cursor.execute("""
+                                INSERT INTO PODCASTER (email)
+                                VALUES (%s)
+                            """, [email])
+                    # if(role == "ARTIST"):
+                    #     with connection.cursor() as cursor:
+                    #         cursor.execute("""
+                    #             INSERT INTO ARTIST (id, email_akun, id_pemilik_hak_cipta)
+                    #             VALUES (%s)
+                    #         """, [email])
+                    
+                return redirect('main:login')
     return render (request, 'register_pengguna.html')
 
 def langganan_paket(request):
