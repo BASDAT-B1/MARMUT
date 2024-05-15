1a063fab047fb5ff97e53bf68a5e05ee1e6deebb
mhmmdmadhanip <muhammad.madhani@ui.ac.id>
2024-05-15 16:01:28 +0700

    Add Detail Playlist

--- kelola_playlist/urls.py
+++ kelola_playlist/urls.py
@@ -7,7 +7,7 @@ app_name = 'kelola_playlist'
 
 urlpatterns = [
     path('add_playlist/', add_playlist, name='add_playlist'),
-    path('detail_playlist/', detail_playlist, name='detail_playlist'),
+    path('detail_playlist/<str:id_playlist>', detail_playlist, name='detail_playlist'),
     path('playlist/', playlist, name='playlist'),
     path('no_playlist/', no_playlist, name='no_playlist'),
 ]
